3f491e3e7d868599d9e8ed26cbeb86838d399e54
csh <tmdgml1754@naver.com>
2024-01-09 19:13:20 +0900

    test graph

--- frontend/src/router/routes.tsx
+++ frontend/src/router/routes.tsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainPage from "../pages/MainPage";
+import CalendarPage from "../pages/CalendarPage";
 const router = createBrowserRouter([
     {
-        path: "",
-        element: <MainPage />,
+        path: "/Calendar",
+        element: <CalendarPage />,
       },
 ]);
 export default router;
