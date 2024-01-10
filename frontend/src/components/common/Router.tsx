355dcd5aab14ecaf5ebb1d6d0f04be323a37fe7b
csh <tmdgml1754@naver.com>
2024-01-10 14:36:22 +0900

    add calendar page

--- frontend/src/components/common/Router.tsx
+++ frontend/src/components/common/Router.tsx
@@ -2,11 +2,12 @@ import { Route, Routes } from 'react-router-dom';
 import EmotionResult from '../../pages/EmotionResult';
 import MainPage from '../../pages/MainPage';
 import CalendarPage from '../../pages/CalendarPage'; 
+import LoginPage from '../../pages/LoginPage';
 
 function Router() {
     return (
         <Routes>
-            <Route path='/' element={<CalendarPage />}></Route>
+            <Route path='/' element={<LoginPage />}></Route>
             <Route path='/result/:diaryId' element={<EmotionResult/>}></Route>
             <Route path='/calendar' element={<CalendarPage/>}></Route>
         </Routes>
